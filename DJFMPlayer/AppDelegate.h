86c35510e6fbf2de8525423533eec8796ac22ebb
dingjianjaja <dingjianjaja@qq.com>
2018-12-06 21:30:18 +0800

    初始化

---
+++ DJFMPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJFMPlayer
+//
+//  Created by dingjianjaja on 2018/12/6.
+//  Copyright © 2018年 dingjianjaja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJFMPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJFMPlayer
+//
+//  Created by dingjianjaja on 2018/12/6.
+//  Copyright © 2018年 dingjianjaja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
